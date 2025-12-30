2059fe9e792aad3b5f9119b1606d9fab73d840a0
jislas621-design <jislas621@gmail.com>
2025-12-30 10:55:29 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ export default function App() {
     // 🔒 Guardar nombre solo una vez
     if (!nombreBloqueado) {
       localStorage.setItem("nombreInvitado", nombre);
-      setNombreBloqueado(false);
+      setNombreBloqueado(true);
     }
 
     // 🔢 Contar TODAS las bebidas alcohólicas del invitado
